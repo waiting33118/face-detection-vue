7a049733d902a9432e337008b427dcf2a4f5bab1
Tony Chung <waiting33118@gmail.com>
2020-11-22 22:55:03 +0800

    feat: edit deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:waiting33118/face-detection-vue.git main:gh-pages
+git push -f git@github.com:waiting33118/face-detection-vue.git master:gh-pages
 
 cd -
