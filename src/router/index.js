4d566f986a04a33941211463ccc0e2c2c86d121d
Tony Chung <waiting33118@gmail.com>
2020-11-22 22:06:58 +0800

    feat: change root route

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Video from '@/views/Video.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'Video',
+    component: Video
   }
 ]
 
